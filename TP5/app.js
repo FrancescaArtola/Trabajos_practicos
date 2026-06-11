08d28fc17ae9821aea1c65118c0c84751933d178
Francesca Artola <50525485@ivt.edu.ar>
2026-06-11 08:24:42 -0300

    Update app.js

--- TP5/app.js
+++ TP5/app.js
@@ -1,8 +1,21 @@
-let numero1 = 25;
-let numero2 = 23;
-let resultado 
+let numero1 = 10;
+let numero2 = 10;
+let resta = 0;
+let suma = 0;
+let multiplicacion = 0;
+let division = 0;
     
-resultado = numero1 + numero2;
+resta=numero1-numero2
+suma=numero1+numero2
+multiplicacion=numero1*numero2
+division=numero1/numero2
 
-console.log(resultado);
-alert(resultado)
+console.log("el resultado de la division es:"+ division);
+console.log("el resultado de la multiplicacion es:" + multiplicacion)
+alert("el resultado de la suma es:" + suma)
+alert("el resultado de la resta es:" + resta)
+
+
+let pais = "Argentina"
+console.long(pais)
+
