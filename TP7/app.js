c0632c74e346dde375dd4644e91fc39014b0c8cb
alumno <alumno@Instituto.local>
2026-07-30 09:12:41 -0300

    30/7

---
+++ TP7/app.js
@@ -0,0 +1,11 @@
+let ej1 = document.querySelector ('#ej1')
+let botonej1 = document.querySelector ('#botonej1')
+let edad = 15
+botonej1.onclick = function(){
+    //ej1.textcontent = 'resultado'
+    if (edad >=18) {
+        ej1.textContent =  'sos mayor de edad'
+    } else {
+        ej1.textContent = 'sos menor de edad'
+    }
+}
