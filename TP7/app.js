430a4278a7b04c1c567dd8a4a14dd22d2031fcc0
alumno <50525485@ivt.edu.ar>
2026-08-10 11:13:44 -0300

    10/08

--- TP7/app.js
+++ TP7/app.js
@@ -9,3 +9,25 @@ botonej1.onclick = function(){
         ej1.textContent = 'sos menor de edad'
     }
 }
+
+let nombredeusuario = "Nahuel"
+let ejercicio2 = document.querySelector("#eje2")
+let botoneje2 = document.querySelector("#eje2")
+botoneje2.onclick = function () {
+    if(nombredeusuario == "Nahuel") {
+        ejercicio2.textContent = "bienvenido Nahuel, como estas?"
+    }
+    else {ejercicio2.textContent = "bienvenido usuario"
+ }
+}
+
+let nombredeusuario = "marcos" +"nahuel"
+let ejercicio3 = document.querySelector ("#eje3")
+let botoneje3 = document.querySelector ("#eje")
+botoneje3.onclick = function (){
+if(nombredeusuario == "marcos" + "nahuel") {
+ejercicio3.textContent = "bienvenido marcos" + "bienvenido nahuel"
+}
+else {ejercicio3.textContent = "bienvenido usuario" 
+}
+}
